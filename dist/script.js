2b1840f23ee32fce241276cbad4c35741bcd8ebd
jeremy <a27262354@gmail.com>
2020-12-09 16:25:20 +0800

    新增函式

--- dist/script.js
+++ dist/script.js
@@ -1,47 +1,56 @@
-const cors = "https://cors-anywhere.herokuapp.com/";
-const endpoint = "https://gis.taiwan.net.tw/XMLReleaseALL_public/activity_C_f.json";
-$.ajax({
-  type: "GET",
-  url: cors+endpoint,
-  dataType: "json",
-  success:function(res){
-    //console.log(res);
-    tododata = (res.XML_Head.Infos.Info).sort(function (a, b) {
-    return a.Start < b.Start ? 1 : -1;
-    }); //大到小
-    tododata=(filterByData(tododata,'2020-12')); //尋找日期為12月
-    //console.log(tododata);
-    //var data=(res);
-    info(tododata);
-  },
-  error: function (thrownError) {
-    let contain=document.querySelector(".container");
-    contain.innerHTML=(thrownError.responseText);
-    }
+$(function(){
+  var today=new Date();
+  const yearmoth=today.getFullYear()+"-"+(today.getMonth()+1); //當年當月變數
+  document.querySelector('h3').innerHTML=yearmoth+"月活動，資料來源:政府開放平台";
+  let container=document.querySelector(".container");
+  const cors = "https://cors-anywhere.herokuapp.com/"; //cors解決方法
+  const endpoint = "https://gis.taiwan.net.tw/XMLReleaseALL_public/activity_C_f.json"; //政府JSON資料
+  
+  $.ajax({
+    type: "GET",
+    url: cors+endpoint,
+    dataType: "json",
+    success:function(res){
+      //console.log(res);
+      //大到小開始日期排序
+      tododata = (res.XML_Head.Infos.Info).sort(function (a, b) {
+      return a.Start < b.Start ? 1 : -1;
+      }); 
+
+      tododata=(filterByData(tododata,yearmoth)); //篩選日期為當月
+      //console.log(tododata);
+      //var data=(res);
+      DoMethod(tododata);
+    },
+    error: function (thrownError) {
+      container.innerHTML=(thrownError.responseText);
+      }
+  });
+
+  //篩選日期函式
+  function filterByData(aim, Year) {
+  return aim.filter(function(item){return item.Start.substr(0,7) == Year})
+  }
+  //處理資料函式
+  function DoMethod(data){
+      container.innerHTML="";
+    var picurl="./sea.jpg"; //picurl預設
+      for(let i=0;i<100;i++)
+      {
+          let info=data[i];
+          const card = document.createElement("div");  //創建物件
+          card.className="card"; // 加入class
+          card.innerHTML+=
+            "<div class='icon'><div class='pic' style='background-image: url("+(info.Picture1!=""?info.Picture1:picurl)+")'/style></div>"
+            +"<div class='tag'>"+info.Region+"</div>"
+            +"<div class='bookbtn'><i class='far fa-bookmark'></i></div></div>"
+            +"<div class='top'><div class='title'>"+(info.Name.substr(18,1)!=""?info.Name.substr(0,18)+"...":info.Name.substr(0,18))+"</div>"
+            +"<div class='adr'>"+info.Town+"-"+info.Add+"<div class='notic'>"+info.Cycle+"</div></div>"
+            +"<div class='time'>日期:"+info.Start.substr(0,10)+'-'+info.End.substr(0,10)+"</div>"
+            +"<div class='text'>說明:<br>"+info.Description.substr(0,100)+"...</div>"
+            +"<div class='bluebar'></div></div>"
+            ;
+          container.appendChild(card);
+  }}
+    
 });
-
-function filterByData(aim, Year) {
-return aim.filter(function(item){return item.Start.substr(0,7) == Year})
-}
-
-function info(data){
-  let contain=document.querySelector(".container");
-    contain.innerHTML="";//先把畫面清空
-  var picurl="./sea.jpg";
-    for(let i=0;i<100;i++)
-    {
-        let product=data[i];
-        const card = document.createElement("div");  //創建物件
-        card.className="card"; // 加入class
-        card.innerHTML+=
-          "<div class='icon'><div class='pic' style='background-image: url("+(product.Picture1!=""?product.Picture1:picurl)+")'/style></div>"
-          +"<div class='tag'>"+product.Region+"</div>"
-          +"<div class='bookbtn'><i class='far fa-bookmark'></i></div></div>"
-          +"<div class='top'><div class='title'>"+(product.Name.substr(18,1)!=""?product.Name.substr(0,18)+"...":product.Name.substr(0,18))+"</div>"
-          +"<div class='adr'>"+product.Town+"-"+product.Add+"<div class='notic'>"+product.Cycle+"</div></div>"
-          +"<div class='time'>日期:"+product.Start.substr(0,10)+'-'+product.End.substr(0,10)+"</div>"
-          +"<div class='text'>說明:<br>"+product.Description.substr(0,100)+"...</div>"
-           +"<div class='bluebar'></div></div>"
-          ;
-        contain.appendChild(card);
-}}
