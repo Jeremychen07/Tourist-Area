5ee724c5c0564dc0d324feb4236112d321ec6d02
jeremy <a27262354@gmail.com>
2020-12-23 00:38:49 +0800

    新增日期判斷

--- dist/script.js
+++ dist/script.js
@@ -2,11 +2,13 @@ $(function(){
   const today=new Date();
   const yearmoth=today.getFullYear(); //當年變數  +"-"+(today.getMonth()+1)
   const container=document.querySelector(".container"); //內容
-  const cors = "https://cors-anywhere.herokuapp.com/"; //cors解決方法
-  const endpoint ="https://gis.taiwan.net.tw/XMLReleaseALL_public/activity_C_f.json"; //政府JSON資料
-  //const endpoint = "https://jeremychen07.github.io/Tourist-Area/opendata.json" //github
+  //const cors = "https://cors-anywhere.herokuapp.com/"; //cors解決方法
+  //const endpoint ="https://gis.taiwan.net.tw/XMLReleaseALL_public/activity_C_f.json"; //政府JSON資料
+  const endpoint = "https://jeremychen07.github.io/Tourist-Area/opendata.json" //github
 
   document.querySelector('h3').innerHTML="<span style='color:red'>"+yearmoth+"</style></span> 年活動，資料來源:政府開放平台";  //首頁標題名稱
+  const bigindata=document.querySelector("#bigindate"); //開始日期
+  const enddate=document.querySelector("#enddate"); //結束日期
 
   //使用者背景顏色變更
   const inputs=document.querySelector("#base");
@@ -15,7 +17,7 @@ $(function(){
   }
   inputs.addEventListener("change",changeHandler);
 
-  //下拉是選單
+  //下拉式選單
   let choose=document.querySelector("#choose");
   let citySelect=document.querySelector("#city");
   let taiwan=["臺北市","新北市","桃園市","臺中市","臺南市","高雄市","新竹縣","苗栗縣","彰化縣","南投縣","雲林縣","嘉義縣","屏東縣","宜蘭縣","花蓮縣","臺東縣","澎湖縣","金門縣","連江縣"];
@@ -28,17 +30,26 @@ $(function(){
   // ajax請求
   $.ajax({
     type: "GET",
-    url: cors+endpoint, // cors解決
+    url: endpoint, // cors解決 cors+
     dataType: "json",
     success:function(res){
       container.innerHTML="載入完成，請輸入您的地區。";
       choose.addEventListener("click",clickHandler);
       function clickHandler(){
+        if(Date.parse(bigindata.value).valueOf()>Date.parse(enddate.value).valueOf())
+        return container.innerHTML="結束日期必須大於開始日期!!";
+        
         let datacity=citySelect.value;
         let data=(res.XML_Head.Infos.Info).sort(function (a, b) {
           return a.Start < b.Start ? 1 : b.Start <  a.Start ? -1 : 0;
           }); 
-        data=data.filter(item=>item.Start.substr(0,4) == yearmoth && item.Region==datacity);
+         
+        data=data.filter(item=>{
+          return  Date.parse(bigindata.value).valueOf() <= Date.parse(item.Start).valueOf() //比對日期
+                  && Date.parse(enddate.value).valueOf() >= Date.parse(item.End).valueOf() 
+                  && item.Region==datacity;
+          
+        });
         doMethod(data);
       }
     },
@@ -62,8 +73,10 @@ $(function(){
           <div class='tag'>${info.Region}</div>
           <div class='bookbtn'><i class='far fa-bookmark'></i></div></div>
           <div class='top'><div class='title'>${info.Name}</div>
-          <div class='adr'>${info.Town}-${info.Add}<div class='notic'>${info.Cycle}</div></div>
-          <div class='time'>日期:${info.Start.substr(0,10)}-${info.End.substr(0,10)}</div>
+          <div class='adr'>${info.Town}-${info.Add}<br><div class='notic'>${info.Cycle}</div></div>
+          <div class='time'><i class="fas fa-bell"> 
+            <small style="color: #EB5E00"> ${info.Start.substr(0,10)}</small> - <small style="color: #EB5E00">${info.End.substr(0,10)} </small>
+          </i></div>
           <div class='text'>說明:<br>${info.Description.substr(0,100)}...</div>
           </div>
           <div class='bluebar'></div>
