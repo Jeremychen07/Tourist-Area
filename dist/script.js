3abf52c98a6eaa0e4303b6f989f5279a0f60a92c
jeremy <a27262354@gmail.com>
2022-06-28 21:44:00 +0800

    修正資料邏輯、變數名稱以及function改寫

--- dist/script.js
+++ dist/script.js
@@ -1,88 +1,123 @@
 $(function(){
-  const today=new Date();
-  //const yearmoth=today.getFullYear(); //當年變數  +"-"+(today.getMonth()+1)
-  const container=document.querySelector(".container"); //內容
-  //const cors = "https://cors-anywhere.herokuapp.com/"; //cors解決方法
-  //const endpoint ="https://gis.taiwan.net.tw/XMLReleaseALL_public/activity_C_f.json"; //政府JSON資料
+  const container = document.querySelector(".container"); //內容
+  // const cors = "https://cors-anywhere.herokuapp.com/"; //cors解決方法
+  //const endpoint = "https://gis.taiwan.net.tw/XMLReleaseALL_public/activity_C_f.json"; //政府JSON資料
   const endpoint = "https://jeremychen07.github.io/Tourist-Area/opendata.json" //github
 
-  document.querySelector('h3').innerHTML="<span style='color:red'>資料來源:政府開放平台</span>";  //首頁標題名稱 "+yearmoth+"</style> 年活動，
-  const bigindata=document.querySelector("#bigindate"); //開始日期
-  const enddate=document.querySelector("#enddate"); //結束日期
-
-  //使用者背景顏色變更
-  const inputs=document.querySelector("#base");
-  function changeHandler(){
-    document.querySelector(":root").style.setProperty("--"+this.name,this.value);
-  }
-  inputs.addEventListener("change",changeHandler);
+  document.querySelector('h3').innerHTML = "<span style='color:red'>資料來源:政府開放平台</span>";  //首頁標題名稱 "+yearmoth+"</style> 年活動，
+  let bigindata = document.querySelector("#begindate"); //開始日期
+  let enddate = document.querySelector("#enddate"); //結束日期  
 
   //下拉式選單
-  let choose=document.querySelector("#choose");
-  let citySelect=document.querySelector("#city");
-  let taiwan=["臺北市","新北市","桃園市","臺中市","臺南市","高雄市","新竹縣","苗栗縣","彰化縣","南投縣","雲林縣","嘉義縣","屏東縣","宜蘭縣","花蓮縣","臺東縣","澎湖縣","金門縣","連江縣"];
-  let selecttext="";
-  for(var i=0;i<taiwan.length;i++){
-    selecttext+=`<option value=${taiwan[i]}>${taiwan[i]}</option>`;
+  let choose = document.querySelector("#choose");
+  let citySelect = document.querySelector("#city");
+  
+  function mainFn(){
+    setBackgroud();
+    setDate();
+    setArea();
+    loadData();
   }
-  citySelect.innerHTML=selecttext;
+  
+  function setBackgroud(){
+    //使用者背景顏色變更
+    let inputs = document.querySelector("#chooseBackgroud");
+    function changeHandler(){
+      document.querySelector(":root").style.setProperty("--"+this.name,this.value);
+    }
+    inputs.addEventListener("change",changeHandler);
+  }
+  function setDate(){
+    const today = new Date();
+    bigindata.value = today.getFullYear() + '-01-01';
+    enddate.value = today.getFullYear() + '-12-31';
+  }
 
-  // ajax請求
-  $.ajax({
-    type: "GET",
-    url: endpoint, // cors解決 cors+
-    dataType: "json",
-    success:function(res){
-      container.innerHTML="載入完成，請輸入您的地區。";
-      choose.addEventListener("click",clickHandler);
-      function clickHandler(){
-        if(Date.parse(bigindata.value).valueOf()>Date.parse(enddate.value).valueOf())
-        return container.innerHTML="結束日期必須大於開始日期!!";
-        
-        let datacity=citySelect.value;
-        let data=(res.XML_Head.Infos.Info).sort(function (a, b) {
-          return a.Start < b.Start ? 1 : b.Start <  a.Start ? -1 : 0;
-          }); 
-         
-        data=data.filter(item=>{
-          return  Date.parse(bigindata.value).valueOf() <= Date.parse(item.Start).valueOf() //比對日期
-                  && Date.parse(enddate.value).valueOf() >= Date.parse(item.End).valueOf() 
-                  && item.Region==datacity;
-          
-        });
-        doMethod(data);
-      }
-    },
-    error: function (thrownError) {
-      container.innerHTML="載入失敗請稍後再試<br>"+thrownError.responseText;
-      }
-  });
+  function setArea(){
+    let taiwan = ["全部" , "臺北市","新北市","桃園市","臺中市","臺南市","高雄市","新竹縣","苗栗縣","彰化縣","南投縣","雲林縣","嘉義縣","屏東縣","宜蘭縣","花蓮縣","臺東縣","澎湖縣","金門縣","連江縣"];
+    let selecttext = "";
+    for(var i = 0; i < taiwan.length; i++){
+      selecttext += `<option value = ${taiwan[i]}>${taiwan[i]}</option>`;
+    }
+    citySelect.innerHTML = selecttext; 
+  }
+  function loadData (){
+    $.ajax({
+      type : "GET",
+      url : endpoint, // cors解決 cors+
+      dataType : "json",
+      success : function(res){
+        container.innerHTML = "載入完成，請輸入您的地區。";
+        choose.addEventListener("click",function(){
+          if(Date.parse(bigindata.value).valueOf()>Date.parse(enddate.value).valueOf())
+            return container.innerHTML = "結束日期必須大於開始日期!!";    
+          let datacity = citySelect.value;
+          let data = (res.XML_Head.Infos.Info).sort(function (a, b) {
+            return a.Start < b.Start ? 1 : b.Start <  a.Start ? -1 : 0;
+            });      
+          data = data.filter(item =>{
+              return  Date.parse(bigindata.value).valueOf() <= Date.parse(item.Start).valueOf() //比對日期
+                      && Date.parse(enddate.value).valueOf() >= Date.parse(item.End).valueOf();      
+            }
+          );
+          if(datacity != "全部")
+          {
+            data = data.filter(item => {
+                return item.Region == datacity;
+              }
+            );
+          }
+          dealConatiner(data);
+        });        
+      },
+      error: function (thrownError) {
+        container.innerHTML = "載入失敗請稍後再試<br>" + thrownError.responseText;
+        }
+    });
+  }
 
   //處理資料函式
-  function doMethod(data){
-    container.innerHTML="";
-    const picurl="./sea.jpg"; //picurl預設
-      for(let i=0;i<data.length;i++)
-      {
-          let info=data[i];
-          let card = document.createElement("div");  //創建物件
-          card.className="card"; // 加入class
-          card.innerHTML+=
-          `
-          <div class='icon'><div class='pic' style='background-image: url(${info.Picture1!=""?info.Picture1:picurl})'/style></div>
+  function dealConatiner(data){
+    container.innerHTML = "";
+    const picturl="./sea.jpg"; //picturl預設
+    for(let i = 0; i < data.length; i++)
+    {
+        let info = data[i];
+        let card = document.createElement("div");  //創建物件
+        card.className = "card"; // 加入class
+        card.innerHTML +=
+        `
+        <div class='icon'>
+          <div class='pic' style='background-image: url(${info.Picture1 != "" ? info.Picture1 : picturl})'></div>
           <div class='tag'>${info.Region}</div>
-          <div class='bookbtn'><i class='far fa-bookmark'></i></div></div>
-          <div class='top'><div class='title'>${info.Name}</div>
-          <div class='adr'>${info.Town}-${info.Add}<br><div class='notic'>${info.Cycle}</div></div>
-          <div class='time'><i class="fas fa-bell"> 
-            <small style="color: #EB5E00"> ${info.Start.substr(0,10)}</small> - <small style="color: #EB5E00">${info.End.substr(0,10)} </small>
-          </i></div>
-          <div class='text'>說明:<br>${info.Description}...</div>
+          <div class='bookbtn'>
+            <i class='far fa-bookmark'></i>
           </div>
-          <div class='bluebar'></div>
-          `;
-          container.appendChild(card);
-
-  }}
-    
+        </div>
+        <div class='top'>
+          <div class='title'>${info.Name}</div>
+          <div class='adr'>
+            ${info.Town}-${info.Add}
+            <br>
+            <div class='notic'>${info.Cycle}</div>
+          </div>
+          <div class='time'>
+            <i class="fas fa-bell">
+              <small style="color: #EB5E00"> ${info.Start.substr(0,10)}</small> 
+              - 
+              <small style="color: #EB5E00">${info.End.substr(0,10)} </small>
+            </i>
+          </div>
+          <div class='text'>
+            說明:
+            <br>
+            ${info.Description}
+          </div>
+        </div>
+        <div class='bluebar'></div>
+        `;
+        container.appendChild(card);
+    }
+  }  
+  mainFn();
 });
